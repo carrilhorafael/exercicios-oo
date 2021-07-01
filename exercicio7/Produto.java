28704a8769c2558dc85d8c63d9412954451f7791
Carrilho <rafael.carrilho@injunior.com.br>
2021-07-01 19:07:00 -0300

    exercicio 7 completo

---
+++ exercicio7/Produto.java
@@ -0,0 +1,13 @@
+package exercicio7;
+
+public class Produto {
+    String nome;
+    float preco;
+    int estoque;
+
+    Produto (String nome, float preco, int estoque){
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+}
